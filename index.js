a2e2154dc95094165d7366b4a5ad54e1eb4356e7
SHUBHENDU SHUBHAM <ldanie21@mail.ccsf.edu>
2021-03-22 01:18:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,9 +16,15 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-app.get('/', (req,res) => {
-  res.send('Hello to API');
-});
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 // const CONNECTION_URL = 'mongodb+srv://crud:mongo07@cluster0.jxt9i.mongodb.net/MERN';
 const PORT = process.env.PORT|| 5000;
